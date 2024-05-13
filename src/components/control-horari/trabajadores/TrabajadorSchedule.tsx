811e2527eaa74e76f35bb3c80871764a09de062d
vidrebany <vidrebany.git@gmail.com>
2024-05-13 21:45:58 +0200

    solucion error horario

--- src/components/control-horari/trabajadores/TrabajadorSchedule.tsx
+++ src/components/control-horari/trabajadores/TrabajadorSchedule.tsx
@@ -22,10 +22,6 @@ const TrabajadorSchedule = () => {
     const [searchParams] = useSearchParams();
     const code = searchParams.get("code");
 
-
-
-
-
     const [extraHours, setExtraHours] = useState<ExtraHours[]>([]);
     const [hourLimits, setHourLimits] = useState<Record<DayType, { minEntryHour: string, maxEntryHour: string, minExitHour: string, maxExitHour: string }>>({
         laborable: { minEntryHour: '5:00', maxEntryHour: '5:00', minExitHour: '20:30', maxExitHour: '20:30' },
@@ -46,7 +42,7 @@ const TrabajadorSchedule = () => {
         '18:30-19:00', '19:00-19:30', '19:30-20:00',
     ];
 
-    // Utility functions to manage time conversions
+    // Utility function to manage time conversions
     const timeToMinutes = (time: string): number => {
         const [hours, minutes] = time.split(":").map(Number);
         return hours * 60 + minutes;
@@ -65,7 +61,11 @@ const TrabajadorSchedule = () => {
             };
 
             extraHours.forEach(eh => {
-                organizedHours[eh.day_type].push(eh);
+                if (organizedHours[eh.day_type]) {
+                    organizedHours[eh.day_type].push(eh);
+                } else {
+                    organizedHours[eh.day_type] = [eh];
+                }
             });
 
             // Now process each day type separately
@@ -77,39 +77,38 @@ const TrabajadorSchedule = () => {
                 const entryHours = dayHours.filter(eh => eh.is_entry).map(eh => eh.start_hour).sort(sortHours);
                 const exitHours = dayHours.filter(eh => !eh.is_entry).map(eh => eh.end_hour).sort(sortHours);
 
+                // Ensure dayType entry in newHourLimits
+                if (!newHourLimits[dayType]) {
+                    newHourLimits[dayType] = {
+                        minEntryHour: '5:00',
+                        maxEntryHour: '5:00',
+                        minExitHour: '20:30',
+                        maxExitHour: '20:30'
+                    };
+                }
+
                 // Update min and max entry hours
                 if (entryHours.length > 0) {
                     newHourLimits[dayType].minEntryHour = entryHours[0];
                     newHourLimits[dayType].maxEntryHour = entryHours[entryHours.length - 1];
-                } else {
-                    // If there are no entry hours, set the min and max entry hours to 5:00
-                    newHourLimits[dayType].minEntryHour = '5:00'
-                    newHourLimits[dayType].maxEntryHour = '5:00'
                 }
+
                 // Update min and max exit hours
                 if (exitHours.length > 0) {
                     newHourLimits[dayType].minExitHour = exitHours[0];
                     newHourLimits[dayType].maxExitHour = exitHours[exitHours.length - 1];
-                } else {
-                    // If there are no exit hours, set the min and max exit hours to 20:30
-                    newHourLimits[dayType].minExitHour = '20:30'
-                    newHourLimits[dayType].maxExitHour = '20:30'
                 }
             });
-            console.log(newHourLimits)
 
             return newHourLimits;
         });
     }, [extraHours]); // Re-run when extraHours changes
-
 
     const handleExtraHoursChange = async (
         dayType: DayType,
         hourRange: string,
         isChecked: boolean,
     ) => {
-
-
         try {
             const [start_hour, end_hour] = hourRange.split("-");
 
@@ -117,12 +116,8 @@ const TrabajadorSchedule = () => {
             const startMinutes = timeToMinutes(start_hour);
             const endMinutes = timeToMinutes(end_hour);
 
-
             if (isChecked) {
                 const limits = hourLimits[dayType];
-                console.log(dayType)
-                console.log("endhour:", end_hour)
-                console.log("minentryhour:", limits.minEntryHour)
                 const minEntryMinutes = timeToMinutes(limits.minEntryHour);
                 const maxEntryMinutes = timeToMinutes(limits.maxEntryHour);
                 const minExitMinutes = timeToMinutes(limits.minExitHour);
@@ -133,12 +128,6 @@ const TrabajadorSchedule = () => {
                         return;
                     }
                 } else {
-                    console.log("endMinutes:", endMinutes)
-                    console.log("maxEntryHour:", limits.maxEntryHour)
-                    console.log("maxEntryMinutes:", maxEntryMinutes)
-                    console.log("startMinutes:", startMinutes)
-                    console.log("minEntryHour:", limits.minEntryHour)
-                    console.log("minEntryMinutes:", minEntryMinutes)
                     if (endMinutes <= minEntryMinutes || startMinutes <= maxEntryMinutes) {
                         toast.error("La hora de salida no puede ser antes de la hora de entrada más temprana.");
                         return;
@@ -166,7 +155,7 @@ const TrabajadorSchedule = () => {
                         start_hour,
                         end_hour,
                         is_entry: isEntry
-                    }]
+                    }];
                 } else {
                     // If the checkbox is not checked, remove the hour range from the state
                     return prevExtraHours.filter(eh => !(eh.day_type === dayType && eh.start_hour === start_hour && eh.end_hour === end_hour));
@@ -175,16 +164,12 @@ const TrabajadorSchedule = () => {
 
             toast.success(`Horas extras ${isChecked ? 'agregadas' : 'removidas'} con éxito.`);
 
-
-
         } catch (error) {
             toast.error("Error al modificar horario:" + error);
         }
-    }
-
+    };
 
     useEffect(() => {
-
         fetchExtraHours(setExtraHours, code);
 
         if (!selectedTrabajador) {
@@ -193,15 +178,15 @@ const TrabajadorSchedule = () => {
             }
         }
         // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [code, dispatch, selectedTrabajador, workers])
-
-
+    }, [code, dispatch, selectedTrabajador, workers]);
 
     if (!selectedTrabajador) {
-        return <div>
-            <button className="btn btn-primary align-items-start" onClick={() => navigate(`/control-horari/trabajadores/trabajador-details?code=${code}`)}>Volver</button>
-            Ningún trabajador seleccionado
-        </div>;
+        return (
+            <div>
+                <button className="btn btn-primary align-items-start" onClick={() => navigate(`/control-horari/trabajadores/trabajador-details?code=${code}`)}>Volver</button>
+                Ningún trabajador seleccionado
+            </div>
+        );
     }
 
     return (
@@ -219,7 +204,7 @@ const TrabajadorSchedule = () => {
                     onClick={() => setIsEntry(true)}>Horas extra</button>
                 {/*<button
                     className={`btn ${isEntry === false ? "btn-danger" : "btn-outline-danger"}`}
-    onClick={() => setIsEntry(false)}>Salida</button>*/}
+                    onClick={() => setIsEntry(false)}>Salida</button>*/}
             </div>
             <table className="table">
                 <thead>
@@ -237,20 +222,17 @@ const TrabajadorSchedule = () => {
                     <HourRange label="viernes" ranges={hourRanges}
                         extraHours={extraHours.filter(eh => eh.day_type === "viernes")}
                         onChange={handleExtraHoursChange} />
-                        {/*
-                    <HourRange label="sabado" ranges={hourRanges}
+                    {/*<HourRange label="sabado" ranges={hourRanges}
                         extraHours={extraHours.filter(eh => eh.day_type === "sabado")}
                         onChange={handleExtraHoursChange} />
                     <HourRange label="domingo_festivo" ranges={hourRanges}
                         extraHours={extraHours.filter(eh => eh.day_type === "domingo_festivo")}
                         onChange={handleExtraHoursChange} />
-                        */}
+                    */}
                 </tbody>
             </table>
         </div>
-    )
-}
+    );
+};
 
 export default TrabajadorSchedule;
-
-
